bcf26515f048112f017f2b83d64350e7d2d34209
hariz <harizsharizad@gmail.com>
2023-07-12 22:00:57 +0800

    creating the navigation bar

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,3 +1,17 @@
+type NavItemsProps = {
+    title :string
+    url: string
+}
+
+function NavItem(props:NavItemsProps) {
+    const {title,url} = props
+    return (
+        <li>
+            <a className="block px-3 py-2 transition hover:text-teal-500"
+               href={url}>{title}</a>
+        </li>
+    )
+}
 function Navbar() {
     return (
         <div className="flex justify-center mx-auto max-w-7xl h-16 pt-6">
@@ -5,10 +19,9 @@ function Navbar() {
                 <ul className="flex rounded-full bg-white/90 px-3 text-sm font-
 medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5
 backdrop-blur">
-                    <li>
-                        <a className="block px-3 py-2 transition hover:text-teal-500"
-                           href="/">About Me</a>
-                    </li>
+                    <NavItem title={"About Me"} url={"/"} />
+                    <NavItem title={"Projects"} url={"/projects"} />
+                    <NavItem title={"Essays"} url={"/essays"} />
                 </ul>
             </nav>
         </div>
