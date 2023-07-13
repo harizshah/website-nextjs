8138e3111a25320d3106290518455bd39cb9f8a7
hariz <harizsharizad@gmail.com>
2023-07-13 14:56:46 +0800

    seo friendly code

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,23 @@ const inter = Inter({ subsets: ['latin'] })
 export async function getStaticProps() {
   return {
     props: {
-      pageId: "about_me"
+      pageId: "about_me",
+      metadata: {
+        title: "Hariz Shah",
+        description: "Discover the work of Shah on her personal website. ",
+        openGraph: {
+          image: "https://example.com/image.jpg",
+          url: "https://example.com"
+        }
+      }
     }
   }
 }
+
 export default function Home() {
-  return <div>About Me</div>
+  return <div className="mt-16 px-8">
+    <header>
+      <h1 className="font-bold text-4xl text-zinc-800">I&apos;m Hariz Shah</h1>
+    </header>
+  </div>
 }
